b70f2bbc9a764ed1225f8833b380a6b7d0aaacc9
Mukadi Kalume <kingdom@menkar.uberspace.de>
2014-11-21 17:45:21 +0100

    rename favicon

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
 var express = require('express');
 var stylus = require('stylus');
+var connect = require('connect');
+var bodyParser   = require('body-parser');
+var morgan         = require('morgan'); // looger wird zu morgan
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 var app = express();
 
+function compile(str, path){
+    return stylus(str).set('filename', path);
+}
+
 app.set('views', __dirname + '/server/views');
-
-
-// Set our default template engine to "jade"
-// which prevents the need for extensions
-// (although you can still mix and match)
 app.set('view engine', 'jade');
-
-app.set(express.logger('dev'));
-app.use(express.bodyParser());
-
+app.use(morgan('dev'));
+app.use(bodyParser.json())
 app.use(stylus.middleware({
     src: __dirname + '/public',
     compile: compile
-
 }));
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.static(__dirname + '/public'));
+
 
 app.get('/',function(reg,res){
     res.render('index');
